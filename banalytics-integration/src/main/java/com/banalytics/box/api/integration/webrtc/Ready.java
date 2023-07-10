4731b8aaeb31a9c9384d1393120292ceaef0acfe
stserakhau <stserakhau@exadel.com>
2023-07-10 16:31:53 +0200

    refactoring gps tracking

--- banalytics-integration/src/main/java/com/banalytics/box/api/integration/webrtc/Ready.java
+++ banalytics-integration/src/main/java/com/banalytics/box/api/integration/webrtc/Ready.java
@@ -1,8 +1,16 @@
 package com.banalytics.box.api.integration.webrtc;
 
 import com.banalytics.box.api.integration.MessageType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString(callSuper = true)
 public class Ready extends AbstractWebRTCMessage {
+    boolean webRequest;
+
     public Ready() {
         super(MessageType.ready);
     }
